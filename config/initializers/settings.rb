add8b96fbd2616799e3505dbbaa1b09af44dbb4c
Gustavo Vera Scuderi <gvescu@gmail.com>
2017-02-27 16:07:20 -0300

    Gemas básicas y algunas configs

---
+++ config/initializers/settings.rb
@@ -0,0 +1,4 @@
+Rails.application.config.i18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+Rails.application.config.i18n.default_locale = :es
+
+Rails.application.config.time_zone = "America/Asuncion"
